f240cf00a201ccce235dd299b2ac845604445474
Jason J. Ralls <jasonralls@Jasons-MacBook-Pro.local>
2020-08-24 23:10:47 -0600

    new changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
   return (
     <section className="landing">
-    <div className="dark-overlay">
-      <div className="landing-inner">
-        <h1 className="x-large">Social Connector</h1>
-        <p className="lead">
-          Create a social media profile/portfolio, share posts and communicate with others
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Social Connector</h1>
+          <p className="lead">
+            Create a social media profile and share posts and comments with
+            other peeps
         </p>
-        <div className="buttons">
-          <a href="register.html" className="btn btn-primary">Sign Up</a>
-          <a href="login.html" className="btn btn-light">Login</a>
+          <div className="buttons">
+            <Link to='/register' className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to='/login' className="btn btn-light">
+              Login
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
+
+export default Landing
